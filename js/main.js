be53142a79a7935d629e56ac32863e26ff135d66
KasturiDona <kasturighosh@192-168-1-208.tpgi.com.au>
2015-10-20 23:17:54 +1100

    added plant, rain, sun, animations

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@ var app = app || {};
 
 app.animate = function () {
 	requestAnimationFrame( app.animate );
+
+	// app.selectedObject = app.scene.getObjectByName("the_sun");
+	// app.selectedObject.rotation.z += 0.005;
+	
 	app.renderer.render( app.scene, app.camera );
 };
 
@@ -9,13 +13,17 @@ app.animateRain = function () {
 	
 	if (app.raining) {
 		requestAnimationFrame( app.animateRain );
-		app.group.position.y -= 0.5;
+		app.particleSystem.position.y -= 0.5;
+
+		app.selectedObject = app.scene.getObjectByName("plant");
+		app.selectedObject.scale.y += 0.005;
+
 		app.renderer.render( app.scene, app.camera );
 	} else {
 		for ( i = app.scene.children.length - 1; i >= 0 ; i -- ) {
             var obj = app.scene.children[ i ];
             if ( obj.name === "Rain" ){
-            	app.scene.remove(obj);
+            	app.scene.remove( obj );
     		}
         }
 	}
@@ -39,13 +47,15 @@ app.addEventHandlers = function () {
 	});
 
 	$('#rain').on('click', function () {
-		app.scene.remove( app.sphere );
-		app.addWaterDrop(true);
+		var selectedObject = app.scene.getObjectByName( "the_sun" );	
+		app.scene.remove( selectedObject );
+		app.addRain( true );
 	});
 
 	$('#stop_rain').on('click', function () {
-		app.scene.add( app.sphere );
-		app.addWaterDrop(false);
+		var selectedObject = app.scene.getObjectByName( "the_sun" );
+		app.scene.add( selectedObject );
+		app.addRain( false );
 	});
 
 	$('#seed').on('click', function () {
@@ -55,67 +65,98 @@ app.addEventHandlers = function () {
 };
 
 app.addSun = function () {
-	var circleShape = new THREE.SphereGeometry( 10, 16, 16 );
-	var material = new THREE.MeshBasicMaterial({ color: 0xFFD600 });
-	app.sphere = new THREE.Mesh( circleShape, material );
-	app.sphere.position.set(100, 5, 0);
-	app.scene.add( app.sphere );
-	app.renderer.render( app.scene, app.camera );
-	app.animate();
 
-};
+	var loader = new THREE.ObjectLoader();
+	loader.load('/assets/sun.json', function( object ) {
+		object.name = "the_sun";
+		object.position.set( 120, 60, 5 );
+		object.scale.x = 16;
+		object.scale.y = 16;
 
-app.addBox = function () {
-	console.log( "add box called" );
-
-	var boxShape = new THREE.BoxGeometry( 20, 20, 20 );
-	var material = new THREE.MeshBasicMaterial({ color: 0x1A237E, wireframe: true });
-	app.cube = new THREE.Mesh( boxShape, material );
-	app.scene.add( app.cube );
+		app.scene.add( object );
+	});
 
 	app.renderer.render( app.scene, app.camera );
-
-	app.addCircle();
 	app.animate();
 };
 
 app.addPlant = function () {
-	var loader = new THREE.JSONLoader();
-	loader.load('/assets/twig.json', function(object) {
-		var mesh = new THREE.Mesh( object);
-		app.scene.add(mesh);
+	var loader = new THREE.ObjectLoader();
+	loader.load('/assets/grass_plant.json', function( object ) {
+		object.name = "plant";
+		object.traverse( function ( child ) {
+			if ( child instanceof THREE.Mesh ) {
+				child.material.specular = new THREE.Color( 0x00ff00 );
+				child.material.emissive = new THREE.Color( 0x006600 );
+				child.material.color.setHex( 0x00FF00 );
+				child.material.color.setRGB( 0, 1, 0 );
+				child.material.color = new THREE.Color( "rgb(0, 1, 0)" );
+			}
+		});
+
+		object.position.y = 0;
+		app.scene.add( object );
 	});
 };
 
-app.addWaterDrop = function (execute) {
+// app.addWaterDrop = function (execute) {
 	
+// 	app.raining = execute;
+
+// 	app.group = new THREE.Object3D();
+// 	app.group.name = "Rain";
+// 	var loader = new THREE.JSONLoader();
+// 	app.quarterWidth = Math.round( app.width / 30 );
+
+//   	loader.load( "/assets/rain_drop.json", function( geometry ){
+//     var material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+//     var mesh = new THREE.Mesh( geometry, material );
+//     for ( var i = 0; i <= app.width; i += app.quarterWidth ) {
+//       for ( var j = 0; j <= app.height; j += 8 ) {
+//         var instance = mesh.clone();
+//         instance.position.set( i / 2, j, 0 );
+//         app.group.add( instance );
+//       }
+//     }
+//   });
+//   app.group.position.x -= 150;
+//   app.scene.add( app.group );
+//   app.animateRain();
+
+// };
+
+app.addRain = function ( execute ) {
 	app.raining = execute;
 
 	app.group = new THREE.Object3D();
 	app.group.name = "Rain";
-	var loader = new THREE.JSONLoader();
+
+	app.particles = new THREE.SphereGeometry();
 	app.quarterWidth = Math.round( app.width / 30 );
 
-  	loader.load( "/assets/rain_drop.json", function( geometry ){
-    var material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
-    var mesh = new THREE.Mesh( geometry, material );
-    for ( var i = 0; i <= app.width; i += app.quarterWidth ) {
+	for (var p = 0; p <= 2000; p++) {
+    	var particle = new THREE.Vector3( Math.random() * 500 - 250, Math.random() * 500 - 250, Math.random() * 500 - 250 );
+    	app.particles.vertices.push( particle );
+	}
+	app.particleTexture = THREE.ImageUtils.loadTexture( "/assets/raindrop.png" );
+	app.particleMaterial = new THREE.PointsMaterial({ map: app.particleTexture, transparent: true, size: 5 });
+	app.particleSystem = new THREE.Points(app.particles, app.particleMaterial);
+
+	for ( var i = 0; i <= app.width; i += app.quarterWidth ) {
       for ( var j = 0; j <= app.height; j += 8 ) {
-        var instance = mesh.clone();
+        var instance = app.particleSystem.clone();
         instance.position.set( i / 2, j, 0 );
         app.group.add( instance );
       }
     }
-  });
-  app.group.position.x -= 150;
-  app.scene.add( app.group );
-  app.animateRain();
 
+	app.scene.add( app.particleSystem );
+	app.animateRain();
 };
 
 
 app.init = function () {
-	console.log("page is loaded");
+	console.log( "page is loaded" );
 	app.camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 	app.camera.position.z = 200;
@@ -131,6 +172,28 @@ app.init = function () {
 
 	console.log( app.renderer );
 	document.body.appendChild( app.renderer.domElement );
+
+	// var light = new THREE.AmbientLight( 0x0000ff ); 
+	// app.scene.add( light );
+
+	var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5);
+	directionalLight.position.set( 120, 60, 0 );
+	app.scene.add( directionalLight );
+
+	var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5);
+	directionalLight.position.set( 1000, 1, +1000 );
+	app.scene.add( directionalLight );
+
+	// var directionalLight = new THREE.DirectionalLight( 0x00ff00, 1);
+	// directionalLight.position.set( 1000, 1, -1000 );
+	// app.scene.add( directionalLight );
+
+	// var directionalLight = new THREE.DirectionalLight( 0x00ff00, 1);
+	// directionalLight.position.set( 0, 1, -1000 );
+	// app.scene.add( directionalLight );
+
+	var light = new THREE.HemisphereLight( 0x00ff00, 0xffffff, 1 );
+	app.scene.add( light );
 
 	app.controls = new THREE.OrbitControls( app.camera, app.renderer.domElement );
 
